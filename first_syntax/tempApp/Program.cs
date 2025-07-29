8b555db87466c1e758f6540c550b7b1a897e19b9
Abisha Ibnu <mieayam5522@gmail.com>
2025-07-29 11:45:22 +0700

    Initial commit

---
+++ first_syntax/tempApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// using System;
+// namespace idk;
+
+// class Program
+// { 
+
+   static void Main()
+    {
+        Console.WriteLine(ReducedAnd(false, true));
+        Console.WriteLine("heh");
+        // Console.WriteLine("Hello, World!");
+        // bool left = true;
+        // bool right = false;
+        // bool result = ReducedAnd(left, right);
+        // Console.WriteLine($"ReducedAnd({left}, {right}) = {result}");
+    }
+    static bool ReducedAnd(bool left, bool right) =>
+  (left, right) switch
+  {
+      (true, true) => true,
+      (_, _) => false,
+  };
+// }
